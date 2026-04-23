f5501ac2323f31a58c0a91cbcdea8d35a36c05ac
SangMin90 <everminelee@naver.com>
2026-04-23 22:30:28 +0900

    TGNX-40 build: 공통 Gradle 빌드 설정을 루트에 통합

--- batch-server/build.gradle.kts
+++ batch-server/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.5.12"
-	id("io.spring.dependency-management") version "1.1.7"
+	id("org.springframework.boot")
+	id("io.spring.dependency-management")
     kotlin("jvm")
-	kotlin("plugin.spring") version "2.3.0"
+	kotlin("plugin.spring")
 }
 
 group = "com.finance"
@@ -13,10 +12,6 @@ java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
 	}
-}
-
-repositories {
-	mavenCentral()
 }
 
 dependencies {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     kotlin("jvm") version "2.3.10" apply false
+    kotlin("plugin.spring") version "2.3.10" apply false
+    id("org.springframework.boot") version "3.5.12" apply false
+	id("io.spring.dependency-management") version "1.1.7" apply false
 }
 
 allprojects {
     group = "com.finance"
     version = "1.0-SNAPSHOT"
-
-    repositories {
-        mavenCentral()
-    }
 }
